6c9aa5717599c3f62cdb2ab3882f041bb1e3d8e6
Ville Niittunen <ww.niittunen@microsoft.com>
2016-06-06 14:14:06 +0300

    Make the App use meteor's publish/subscribe model

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -2,7 +2,14 @@ import {Meteor} from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import {check} from 'meteor/check';
 
- export const Tasks = new Mongo.Collection('tasks');
+export const Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find();
+  });
+}
 
 Meteor.methods({
   'tasks.insert'(text) {
